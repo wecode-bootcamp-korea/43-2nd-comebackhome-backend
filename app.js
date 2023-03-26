e2ce9e108c15ab78bf39676e5d13f104a3300fe5
trtw9889 <trtw9889@gmail.com>
2023-03-26 16:42:56 +0900

    [43기 임수정]Backend 초기세팅(수정사항반영)

--- app.js
+++ app.js
@@ -1,43 +1,31 @@
-require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
-const appDataSource = require("./api/models/dataSource");
 const route = require("./api/routes");
 const { globalErrorHandler } = require("./api/utils/error");
 
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(morgan("tiny"));
-app.use(route);
+const createApp = () => {
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+  app.use(morgan("tiny"));
+  app.use(route);
 
-app.get("/ping", (req, res) => {
-  res.status(200).json({ message: "pong" });
-});
-
-app.all("*", (req, res, next) => {
-  const error = new Error(`Can't find ${req.originalUrl} on this server!`);
-  error.statusCode = 400;
-
-  next(error);
-});
-
-app.use(globalErrorHandler);
-
-appDataSource
-  .initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  })
-  .catch((error) => {
-    console.error("Error during Data Source initialization", error);
+  app.get("/ping", (req, res) => {
+    res.status(200).json({ message: "pong" });
   });
 
-const PORT = process.env.PORT;
+  app.all("*", (req, res, next) => {
+    const error = new Error(`Can't find ${req.originalUrl} on this server!`);
+    error.statusCode = 400;
 
-app.listen(PORT, () => {
-  console.log(`server is listening on ${PORT}`);
-});
+    next(error);
+  });
+
+  app.use(globalErrorHandler);
+
+  return app;
+};
+
+module.exports = createApp;
