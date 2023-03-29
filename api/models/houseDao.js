4b63059e343848b7ab7f3d259a1e0cd6bddd1110
류승준 <ryuseungjun@lyuseungjun-ui-MacBookPro.local>
2023-03-29 17:18:49 +0900

    [ADD]: 집들이 정보 호출

--- api/models/houseDao.js
+++ api/models/houseDao.js
@@ -1,7 +1,8 @@
 const appDataSource = require("./dataSource")
 
 const getHouseById = async (postsId) => {
-    return await appDataSource.query(`
+    const postData =  await appDataSource.query(
+        `
         SELECT
             posts.id,
             posts.title,
@@ -9,18 +10,32 @@ const getHouseById = async (postsId) => {
             posts.user_id as userId,
             posts.room_size_type_id as roomSizeId,
             users.social_nickname,
-            room_size_types.name,
+            room_size_types.name as roomSize,
             post_images.image_url
             from posts
             JOIN users ON posts.user_id = users.id
             JOIN room_size_types ON posts.room_size_type_id = room_size_types.id
-            JOIN post_images (
-                SELECT
-                
-            )image_coordinates ON post_images.id = image_coordinates.post_image_id
+            JOIN post_images ON posts.id = post_images.post_id
         WHERE posts.id = ?`,
         [postsId]
     )
+
+    const productData = await appDataSource.query(
+        `
+        SELECT
+            products.id,
+            products.image_url,
+            products.name,
+            products.price
+            from products
+        `,
+        
+        postData.productData = productData
+
+
+    )
+
+    
 }
 
 module.exports = {
